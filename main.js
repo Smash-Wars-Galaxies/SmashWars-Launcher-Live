9db6d8997bf2a71783b1b668c56b56df64e33c43
InfiniteAxis <InfiniteAxis@users.noreply.github.com>
2024-08-25 00:35:15 -0400

    Fixed Issues with non-working buttons and fix pointers for correct downloads for patching

--- main.js
+++ main.js
@@ -1,84 +1,64 @@
 const {app, BrowserWindow, ipcMain, dialog} = require('electron');
+require('dotenv').config();
 const log = require('electron-log');
 const {autoUpdater} = require('electron-updater');
 const path = require('path');
 const url = require('url');
-const fs = require('fs');
-
-var setupWindow = null;
-var err;
-
-app.commandLine.appendSwitch("disable-http-cache");
 
 log.transports.file.file = require('os').homedir() + '/SmashSWG-Launcher-log.txt';
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
-//log.info('App starting...');
+log.info('App starting...');
 
 let mainWindow;
 
 function createWindow() {
 	mainWindow = new BrowserWindow({
-		width: 1024,
-		height: 610,
-		useContentSize: true,
-		center: true,
-		resizable: false,
-		fullscreen: false,
-		fullscreenable: false,
-		maximizable: false,
-		minWidth: 1024,
-		minHeight: 610,
-		maxWidth: 1024,
-		maxHeight: 610,
-		transparent: true,
-		show: false,
-		autoHideMenuBar: true,
+		width: 1024, 
+		minWidth: 1024, 
+		height: 600, 
+		minHeight: 600, 
+		show: false, 
+		autoHideMenuBar: true, 
 		frame: false,
-		webPreferences: { 
-			disableBlinkFeatures: "Auxclick",
-			nodeIntegration: true,
-			enableRemoteModule: true,
-			webviewTag: true
-		},
-    		icon: path.join(__dirname, 'img/logo_icon.ico')
-  	});
+		icon: path.join(__dirname, 'img/logo_icon.ico'),
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
 	mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file:',
 		slashes: true
-	 }));
-	  //if (require('electron-is-dev')) mainWindow.webContents.openDevTools();
-	  mainWindow.once('ready-to-show', () => mainWindow.show());
-	  mainWindow.once('closed', () => mainWindow = null);  
+	}));
+	//if (require('electron-is-dev')) mainWindow.webContents.openDevTools();
+	mainWindow.once('ready-to-show', () => mainWindow.show());
+	mainWindow.once('closed', () => mainWindow = null);
 }
 
-app.on('ready', () => setTimeout(createWindow, 100)); // Linux / MacOS transparancy fix
+app.on('ready', createWindow);
 app.on('window-all-closed', () => app.quit());
 
-ipcMain.on('open-directory-dialog', async (event, response) => {
-  const result = await dialog.showOpenDialog({
+ipcMain.on('open-directory-dialog', function (event, response) {
+  dialog.showOpenDialog({
     properties: ['openDirectory']
+  }, function (files) {
+    if (files) event.sender.send(response, files[0])
   });
-  if (result.filePaths[0] != undefined) {
-	  event.sender.send(response, result.filePaths[0]);
-  }
-  log.info('Selected Directory Path', result.filePaths[0]);
 });
 
 autoUpdater.on('update-downloaded', (info) => {
-  autoUpdater.quitAndInstall();
+  autoUpdater.quitAndInstall();  
 });
 
 autoUpdater.on('download-progress', (progress) => {
   mainWindow.webContents.send('download-progress', progress);
-});
+})
 
 autoUpdater.on('update-available', info => {
-  mainWindow.webContents.send('downloading-update', 'Downloading Update ' + info.version);
-});
+  mainWindow.webContents.send('downloading-update', 'Downloading version ' + info.version);
+})
 
 app.on('ready', function()  {
-  if (!require('electron-is-dev'))
-    autoUpdater.checkForUpdates();
+  autoUpdater.checkForUpdates();
 });
--- renderer.js
+++ renderer.js
@@ -1,5 +1,4 @@
-const electron = require('electron');
-const ipc = electron.ipcRenderer;
+const ipc = require('electron').ipcRenderer;
 const shell = require('electron').shell;
 const remote = require('electron').remote;
 const fs = require('fs');
@@ -8,13 +7,12 @@ const server = require('./server');
 const package = require('./package');
 const install = require('./install');
 const path = require('path');
-const log = require('electron-log');
 
 const playBtn = document.getElementById('play');
 const websiteBtn = document.getElementById('web');
 const discordBtn = document.getElementById('disc');
-
 const rightContent = document.getElementById('rightcontent');
+const rightSettings = document.getElementById('rightsettings');
 const folderBox = document.getElementById('folder');
 const browseBtn = document.getElementById('browse');
 const installBtn = document.getElementById('install');
@@ -24,7 +22,6 @@ const modListBox = document.getElementById('modlist');
 const progressBox = document.getElementById('progressbox');
 const progressBar = document.getElementById('progress');
 const progressText = document.getElementById('progresstext');
-const news = document.getElementById('news');
 const minBtn = document.getElementById('minimize');
 const maxBtn = document.getElementById('maximize');
 const closeBtn = document.getElementById('close');
@@ -36,158 +33,156 @@ const versionDiv = document.getElementById('version');
 versionDiv.innerHTML = package.version;
 
 const configFile = require('os').homedir() + '/SmashWarsGalaxies-Launcher.json';
-var config = {folder: 'C:\\SmashWarsGalaxies'};
+var config = { folder: 'C:\\SmashWarsGalaxies' };
 if (fs.existsSync(configFile))
-    config = JSON.parse(fs.readFileSync(configFile));
-
+	config = JSON.parse(fs.readFileSync(configFile));
 folderBox.value = config.folder;
 var needSave = false;
 if (!config.mods) {
-    config.mods = [];
-    needSave = true;
+	config.mods = [];
+	needSave = true;
 }
 if (!config.fps) {
-    config.fps = 30;
-    needSave = true;
+	config.fps = 30;
+	needSave = true;
 }
 fpsSel.value = config.fps;
 if (!config.ram) {
-    config.ram = 750;
-    needSave = true;
+	config.ram = 750;
+	needSave = true;
 }
 ramSel.value = config.ram;
 if (!config.zoom) {
-    config.zoom = 1;
-    needSave = true;
+	config.zoom = 1;
+	needSave = true;
 }
 zoomSel.value = config.zoom;
 if (needSave) saveConfig();
 
 minBtn.addEventListener('click', event => remote.getCurrentWindow().minimize());
+maxBtn.addEventListener('click', event => {
+	var window = remote.getCurrentWindow();
+	if (!window.isMaximized()) window.maximize();
+	else window.unmaximize();
+});
 closeBtn.addEventListener('click', event => remote.getCurrentWindow().close());
 
 playBtn.addEventListener('click', event => {
-    if (playBtn.disabled) return;
-    var fd = fs.openSync(path.join(config.folder, "SWGEmu.exe"), "r");
-    var buf = new Buffer(7);
-    var bytes = fs.readSync(fd, buf, 0, 7, 0x1153);
-    fs.closeSync(fd);
-    fd = null;
-    if (bytes == 7 && buf.readUInt8(0) == 0xc7 && buf.readUInt8(1) == 0x45 && buf.readUInt8(2) == 0x94 && buf.readFloatLE(3) != config.fps) {
-        var file = require('random-access-file')(path.join(config.folder, "SWGEmu.exe"));
-        buf = new Buffer(4);
-        buf.writeFloatLE(config.fps);
-        file.write(0x1156, buf, err => {
-            if (err) alert("Could not modify FPS. Close all instances of the game to update FPS.\n" + ex.toString());
-            file.close(play);
-        })
-    } else {
-        play();
-	    goHome();
-    }
+	if (playBtn.disabled) return;
+	var fd = fs.openSync(path.join(config.folder, "SWGEmu.exe"), "r");
+	var buf = new Buffer(7);
+	var bytes = fs.readSync(fd, buf, 0, 7, 0x1153);
+	fs.closeSync(fd);
+	fd = null;
+	if (bytes == 7 && buf.readUInt8(0) == 0xc7 && buf.readUInt8(1) == 0x45 && buf.readUInt8(2) == 0x94 && buf.readFloatLE(3) != config.fps) {
+		var file = require('random-access-file')(path.join(config.folder, "SWGEmu.exe"));
+		buf = new Buffer(4);
+		buf.writeFloatLE(config.fps);
+		file.write(0x1156, buf, err => {
+			if (err) alert("Could not modify FPS. Close all instances of the game to update FPS.\n" + ex.toString());
+			file.close(play);
+		})
+	} else {
+		play();
+	}
 });
 
-
 function play() {
-    fs.writeFileSync(path.join(config.folder, "swgemu_login.cfg"), `[ClientGame]\r\nloginServerAddress0=${server.address}\r\nloginServerPort0=${server.port}\r\nfreeChaseCameraMaximumZoom=${config.zoom}`);
-    var args = ["--",
-        "-s", "ClientGame", "loginServerAddress0=" + server.address, "loginServerPort0=" + server.port,
-        "-s", "Station", "gameFeatures=34929",
-        "-s", "SwgClient", "allowMultipleInstances=false"];
-    var env = Object.create(require('process').env);
-    env.SWGCLIENT_MEMORY_SIZE_MB = config.ram;
-    const child = process.spawn("SWGEmu.exe", args, {cwd: config.folder, env: env, detached: true, stdio: 'ignore'});
-    child.unref();
+	fs.writeFileSync(path.join(config.folder, "swgemu_login.cfg"), `[ClientGame]\r\nloginServerAddress0=${server.address}\r\nloginServerPort0=${server.port}\r\nfreeChaseCameraMaximumZoom=${config.zoom}`);
+	var args = ["--",
+		"-s", "ClientGame", "loginServerAddress0=" + server.address, "loginServerPort0=" + server.port,
+		"-s", "Station", "gameFeatures=34929",
+		"-s", "SwgClient", "allowMultipleInstances=true"];
+	var env = Object.create(require('process').env);
+	env.SWGCLIENT_MEMORY_SIZE_MB = config.ram;
+	const child = process.spawn("SWGEmu.exe", args, { cwd: config.folder, env: env, detached: true, stdio: 'ignore' });
+	child.unref();
 }
 
 gamesettingsBtn.addEventListener('click', event => {
-    const child = process.spawn("cmd", ["/c", path.join(config.folder, "SWGEmu_Setup.exe")], {cwd: config.folder, detached: true, stdio: 'ignore'});
-    child.unref();
-});
+	const child = process.spawn("cmd", ["/c", path.join(config.folder, "SWGEmu_Setup.exe")], { cwd: config.folder, detached: true, stdio: 'ignore' });
+	child.unref();
+})
 
-function goHome() {
-    rightContent.style.display == 'none';
-    rightContent.style.display = 'block';
-    rightSettings.style.display = 'none';
-    settings.className = "button";
-    newsHeader.style.display = 'block';
+function showSettingsPanel() {
+	rightContent.style.display = 'none';
+	rightSettings.style.display = 'block';
+	settings.className = "button active";
 }
 
 settings.addEventListener('click', event => {
-    if (rightContent.style.display == 'none') {
-        goHome();
-    } else {
-        rightContent.style.display = 'none';
-        rightSettings.style.display = 'block';
-        settings.className = "button active";
-        newsHeader.style.display = 'none';
-    }
+	if (rightContent.style.display == 'none') {
+		rightContent.style.display = 'block';
+		rightSettings.style.display = 'none';
+		settings.className = "button";
+	} else {
+		showSettingsPanel();
+	}
 });
 
-
-home.addEventListener('click', event => goHome());
+home.addEventListener('click', event => {
+	rightContent.style.display = 'block';
+	rightSettings.style.display = 'none';
+	settings.className = "button";
+});
 
 websiteBtn.addEventListener('click', event => shell.openExternal("https://www.twitch.tv/smashley"));
 discordBtn.addEventListener('click', event => shell.openExternal("https://discord.gg/smashley"));
-versionDiv.addEventListener('click', event => remote.getCurrentWindow().toggleDevTools());
 
 browseBtn.addEventListener('click', function (event) {
-    ipc.send('open-directory-dialog', 'selected-directory');
+	ipc.send('open-directory-dialog', 'selected-directory');
 });
 
 folderBox.addEventListener('keyup', event => {
-    config.folder = event.target.value;
-    saveConfig();
+	config.folder = event.target.value;
+	saveConfig();
 });
 
 ipc.on('selected-directory', function (event, path) {
-    folderBox.value = path;
-    config.folder = path;
-    saveConfig();
+	folderBox.value = path;
+	config.folder = path;
+	saveConfig();
 });
 
 fpsSel.addEventListener('change', event => {
-    config.fps = event.target.value;
-    saveConfig();
+	config.fps = event.target.value;
+	saveConfig();
 });
-
 ramSel.addEventListener('change', event => {
-    config.ram = event.target.value;
-    saveConfig();
+	config.ram = event.target.value;
+	saveConfig();
 });
-
 zoomSel.addEventListener('change', event => {
-    config.zoom = event.target.value;
-    saveConfig();
+	config.zoom = event.target.value;
+	saveConfig();
 });
 
-installBtn.addEventListener('click', function(event) {
-    if (installBtn.disabled = false) return;
-    installBtn.disabled = true;
-    ipc.send('open-directory-dialog', 'install-selected');
+installBtn.addEventListener('click', function (event) {
+	if (installBtn.disabled = false) return;
+	installBtn.disabled = true;
+	ipc.send('open-directory-dialog', 'install-selected');
 });
 
-cancelBtn.addEventListener('click', function(event) {
-    install.cancel();
-    enableAll();
-    progressBox.style.display = 'none';
-});
+cancelBtn.addEventListener('click', function (event) {
+	install.cancel();
+	enableAll();
+	progressBox.style.display = 'none';
+})
 
 ipc.on('install-selected', function (event, path) {
-    log.info('ipc on install-selected called');
-    disableAll();
-    resetProgress();
-    install.install(path, config.folder, config.mods);
+	disableAll();
+	resetProgress();
+	install.install(path, config.folder, config.mods);
 });
 
 ipc.on('downloading-update', function (event, text) {
-    versionDiv.innerHTML = text;
-    disableAll();
+	versionDiv.innerHTML = text;
+	disableAll();
 });
 
-ipc.on('download-progress', function(event, info) {
-    install.progress(info.transferred, info.total);
-});
+ipc.on('download-progress', function (event, info) {
+	install.progress(info.transferred, info.total);
+})
 
 var lastCompleted = 0;
 var lastTime = new Date();
@@ -195,111 +190,113 @@ var rate = 0;
 var units = " B/s";
 
 function resetProgress() {
-    lastCompleted = 0;
-    lastTime = new Date();
-    rate = 0;
+	lastCompleted = 0;
+	lastTime = new Date();
+	rate = 0;
 }
 
-install.progress = function(completed, total) {
-    var time = new Date();
-    var elapsed = (time - lastTime) / 1000;
-    if (elapsed >= 1) {
-        var bytes = completed - lastCompleted;
-        units = " B/s";
-        rate = bytes / elapsed;
-        if (rate > 1024) {
-            rate = rate / 1024;
-            units = " KB/s";
-        }
-        if (rate > 1024) {
-            rate = rate / 1024;
-            units = " MB/s";
-        }
-        lastCompleted = completed;
-        lastTime = time;
-    }
-    if (progressBox.style.display == 'none') progressBox.style.display = 'block';
-    progressText.innerHTML = Math.trunc(completed * 100 / total) + '% (' + rate.toPrecision(3) + units + ')';
-    progressBar.style.width = (completed * 100 / total) + '%';
-    if (completed == total) {
-        enableAll();
-        progressBox.style.display = 'none';
-    }
+install.progress = function (completed, total) {
+	var time = new Date();
+	var elapsed = (time - lastTime) / 1000;
+	if (elapsed >= 1) {
+		var bytes = completed - lastCompleted;
+		units = " B/s";
+		rate = bytes / elapsed;
+		if (rate > 1024) {
+			rate = rate / 1024;
+			units = " KB/s";
+		}
+		if (rate > 1024) {
+			rate = rate / 1024;
+			units = " MB/s";
+		}
+		lastCompleted = completed;
+		lastTime = time;
+	}
+	if (progressBox.style.display == 'none') progressBox.style.display = 'block';
+	progressText.innerHTML = Math.trunc(completed * 100 / total) + '% (' + rate.toPrecision(3) + units + ')';
+	progressBar.style.width = (completed * 100 / total) + '%';
+	if (completed == total) {
+		enableAll();
+		progressBox.style.display = 'none';
+	}
 }
 
-install.modList = function(mods) {
-    modListBox.innerHTML = "";
-    for (var mod of mods) {
-        var checkbox = document.createElement('input');
-        checkbox.type = "checkbox";
-        checkbox.value = mod;
-        checkbox.id = mod.replace(/[^a-zA-Z]/g, "");
-        checkbox.checked = config.mods.includes(mod);
-        checkbox.onchange = modListChanged;
-        checkbox.disabled = true;
-        var label = document.createElement('label');
-        label.htmlFor = checkbox.id;
-        label.appendChild(document.createTextNode(mod));
-        var li = document.createElement('li');
-        li.appendChild(checkbox);
-        li.appendChild(label);
-        modListBox.appendChild(li);
-    }
+install.modList = function (mods) {
+	modListBox.innerHTML = "";
+	for (var mod of mods) {
+		var checkbox = document.createElement('input');
+		checkbox.type = "checkbox";
+		checkbox.value = mod;
+		checkbox.id = mod.replace(/[^a-zA-Z]/g, "");
+		checkbox.checked = config.mods.includes(mod);
+		checkbox.onchange = modListChanged;
+		checkbox.disabled = true;
+		var label = document.createElement('label');
+		label.htmlFor = checkbox.id;
+		label.appendChild(document.createTextNode(mod));
+		var li = document.createElement('li');
+		li.appendChild(checkbox);
+		li.appendChild(label);
+		modListBox.appendChild(li);
+	}
 }
 
 function modListChanged() {
-    config.mods = [];
-    for (var child of modListBox.children) {
-        if (child.children[0].checked) config.mods.push(child.children[0].value);
-    }
-    saveConfig();
-    disableAll();
-    resetProgress();
-    install.install(config.folder, config.folder, config.mods);
+	config.mods = [];
+	for (var child of modListBox.children) {
+		if (child.children[0].checked) config.mods.push(child.children[0].value);
+	}
+	saveConfig();
+	disableAll();
+	resetProgress();
+	install.install(config.folder, config.folder, config.mods);
 }
 
-fullscanBtn.addEventListener('click', function(event) {
-    if (fullscanBtn.disabled) return;
-    disableAll();
-    resetProgress();
-    install.install(config.folder, config.folder, config.mods, true);
+fullscanBtn.addEventListener('click', function (event) {
+	if (fullscanBtn.disabled) return;
+	disableAll();
+	resetProgress();
+	install.install(config.folder, config.folder, config.mods, true);
 });
 
 if (fs.existsSync(path.join(config.folder, 'bottom.tre'))) {
-    disableAll();
-    resetProgress();
-    install.install(config.folder, config.folder, config.mods);
+	disableAll();
+	resetProgress();
+	install.install(config.folder, config.folder, config.mods);
 } else {
-    playBtn.disabled = true;
-    fullscanBtn.disabled = true;
-    install.getManifest();
-    settings.click();
+	playBtn.disabled = true;
+	fullscanBtn.disabled = true;
+	install.getManifest();
+	settings.click();
 }
 
 function disableAll() {
-    folderBox.disabled = true;
-    fullscanBtn.disabled = true;
-    installBtn.disabled = true;
-    playBtn.disabled = true;
-    browseBtn.disabled = true;
-    for (var child of modListBox.children) {
-        child.children[0].disabled = true;
-    }
+	folderBox.disabled = true;
+	fullscanBtn.disabled = true;
+	installBtn.disabled = true;
+	playBtn.disabled = true;
+	browseBtn.disabled = true;
+	for (var child of modListBox.children) {
+		child.children[0].disabled = true;
+	}
 }
 
 function enableAll() {
-    folderBox.disabled = false;
-    fullscanBtn.disabled = false;
-    installBtn.disabled = false;
-    playBtn.disabled = false;
-    browseBtn.disabled = false;
-    for (var child of modListBox.children) {
-        child.children[0].disabled = false;
-    }
+	folderBox.disabled = false;
+	fullscanBtn.disabled = false;
+	installBtn.disabled = false;
+	playBtn.disabled = false;
+	browseBtn.disabled = false;
+	for (var child of modListBox.children) {
+		child.children[0].disabled = false;
+	}
 }
 
 function saveConfig() {
-    fs.writeFileSync(configFile, JSON.stringify(config));
+	fs.writeFileSync(configFile, JSON.stringify(config));
 }
 
+versionDiv.addEventListener('click', event => remote.getCurrentWebContents().openDevTools());
 
+showSettingsPanel();
